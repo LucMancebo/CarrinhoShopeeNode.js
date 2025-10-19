528b3c9fb5cfbeec68c600199a181ab85d598a30
Lucas Mancebo <lucmancebo15@gmail.com>
2025-10-19 00:42:09 -0300

    feat(cart): implementa casos de uso principais do carrinho

---
+++ src/services/cart.js
@@ -0,0 +1,21 @@
+
+
+// Adicionar item
+async function addItem(userCart) {
+    
+}
+
+//deletar item
+async function deleteItem(userCart, name) {
+    
+}
+
+//remover item
+async function removeItem(userCart, index) {
+    
+}
+
+//calcular total
+async function calculateTotal(userCart) {
+    
+}
